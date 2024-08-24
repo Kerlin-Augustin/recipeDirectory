de92b329e6a665901f27d647bf8c2d75c2bbb697
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-08-24 16:41:51 -0400

    recipes

--- cooking-ninja/src/pages/home/Home.js
+++ cooking-ninja/src/pages/home/Home.js
@@ -1,9 +1,16 @@
 import './Home.css'
+import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
+
 
 export default function Home(){
+  const { data , isPending, error } = useFetch('http://localhost:3000/recipes')
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data}/>}
     </div>
   )
 }
